80fe7409c7bf726f1458dedf1f0ca0d279c1184b
randomaru <m.duval@yandex.ru>
2020-01-20 21:52:30 +0300

    push to stack B -> OK

--- src/checker.c
+++ src/checker.c
@@ -49,10 +49,50 @@ void	rr_reverse(t_num **head, t_num **tail)
 	tmp->prev = NULL;
 	tmp->next = *head;
 	*head = tmp;
-	tmp = *head;
+/*	tmp = *head;
 	while (tmp != NULL)
 	{
 		ft_printf("%2i %2i\n", tmp->number, tmp->index);
 		tmp = tmp->next;
+	}*/
+}
+
+void	push(char c, t_num **head_a, t_num **head_b)
+{
+	t_num *tmp;
+	t_num *tmp_next;
+	if (c == 'a')
+	{
+		tmp = *head_b;
+		*head_b = (*head_b)->next;
+		(*head_a)->prev = tmp;
+		tmp->next = *head_a;
+		*head_a = tmp;
+//		*head_b = NULL;
 	}
+	if (c == 'b')
+	{
+		tmp = *head_a;
+		tmp_next = (*head_a)->next;
+		if (*head_b == NULL)
+		{
+			*head_b = *head_a;
+			tmp_next->prev = NULL;
+			(*head_b)->prev = NULL;
+			(*head_b)->next = NULL;
+			*head_a = tmp_next;
+		}
+		else
+		{
+			tmp_next->prev = NULL;
+			tmp->prev = NULL;
+			tmp->next = *head_b;
+			(*head_b)->prev = tmp;
+			(*head_b) = tmp;
+			*head_a = tmp_next;
+		}
+
+
+	}
+
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tamarant <tamarant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/16 13:28:17 by tamarant          #+#    #+#             */
-/*   Updated: 2020/01/20 19:20:52 by mac              ###   ########.fr       */
+/*   Updated: 2020/01/20 21:50:00 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 int		main(int argc, char **argv)
 {
 	t_num	*head;
+	t_num	*head_b;
 	t_num	*tail;
 	t_num	*head_tmp;
 	t_num	*new;
@@ -23,6 +24,7 @@ int		main(int argc, char **argv)
 
 	new = NULL;
 	head = NULL;
+	head_b = NULL;
 	tail = NULL;
 	i = 1;
 	if (!(new = new_t_num()))
@@ -62,7 +64,33 @@ int		main(int argc, char **argv)
 		ft_printf("%2i %2i\n", tail->number, tail->index);
 		tail = tail->prev;
 	}*/
-	rr_reverse(&head, &tail);
+//	rr_reverse(&head, &tail);
+	push('b', &head, &head_b);
+	push('b', &head, &head_b);
+
+	t_num *tmp_head_a = head;
+	t_num *tmp_head_b = head_b;
+	while (tmp_head_a || tmp_head_b)
+	{
+		if (tmp_head_b == NULL && tmp_head_a)
+		{
+			ft_printf("%7i | %7c\n", tmp_head_a->number, ' ');
+			tmp_head_a = tmp_head_a->next;
+		}
+		else if (tmp_head_a == NULL && tmp_head_b)
+		{
+			ft_printf("%7c | %7i\n", ' ', tmp_head_b->number);
+			tmp_head_b = tmp_head_b->next;
+		}
+		else if (tmp_head_a && tmp_head_b)
+		{
+			ft_printf("%7i | %7i\n", tmp_head_a->number, tmp_head_b->number);
+			tmp_head_a = tmp_head_a->next;
+			tmp_head_b = tmp_head_b->next;
+		}
+
+	}
+	ft_printf("\n%7s | %7s\n", "stack A", "stack B");
 	final_free(&head);
 	return (0);
 }
